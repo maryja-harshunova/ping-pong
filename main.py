2dd189ff1cf7b42cbd5db6a6c000d6d724a81877
maryja-harshunova <spiakota@outlook.com>
2024-03-23 15:05:40 +0300

    create GameSprite class

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ timer = time.Clock()
 
 run = True
 
+class GameSprite(sprite.Sprite):
+    def __init__ (self, img, x, y, speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(img), width, height)
+        self.speed = 0
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 while run:
     for e in event.get():
         if e.type == QUIT:
