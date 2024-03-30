049bf40c03fad708d2b5271abfeb432f979a4ba7
maryja-harshunova <spiakota@outlook.com>
2024-03-30 14:26:33 +0300

    add images, create objects

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ run = True
 class GameSprite(sprite.Sprite):
     def __init__ (self, img, x, y, speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(img), width, height)
-        self.speed = 0
+        self.image = transform.scale(image.load(img), (width, height))
+        self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -37,11 +37,21 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y + self.rect.height < SCREENSIZE[1]:
             self.rect.y += self.speed
 
+platform_left = Player("platform.png", 20, 320, 4, 25, 99)
+platform_right = Player("platform.png", 660, 320, 4, 25, 99)
+
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-
+    
     window.fill(BACKCOLOR)
+    
+    platform_left.reset()
+    platform_right.reset()
+
+    platform_left.update_left()
+    platform_right.update_right()
+
     display.update()
     timer.tick(60)
